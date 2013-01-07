cb43c1222776954b805e47ff2c4e2b6d4282716f
Kristof Peeters <circafreak@hotmail.com>
2013-01-07 03:13:51 +0100

    mutationplot

--- src/plots/mutation/PlotMutation.java
+++ src/plots/mutation/PlotMutation.java
@@ -25,8 +25,6 @@ import factory.PathFactory;
 
 public class PlotMutation {
 
-	private static final Params params = new Params();
-
 	private static final String problemFilePath = "../genetic/datafiles/xqf131.tsp"; //optimal length 564
 	private static final String outputPath = "../genetic/result/result-path-mutation-";
 	private static final String outputAdjacency = "../genetic/result/result-adj-mutation-";
@@ -39,63 +37,86 @@ public class PlotMutation {
 
 	public static void makePlot() {
 
-		int nbTimes = 1;
-		double step = 0.05;
-		double max = 0.1;
+		long before = System.currentTimeMillis();
+		
+		int nbTimes = 8;
+		double step = 0.02;
+		double max = 0.5;
 		
 		int randomSeed = (new Random()).nextInt();
 
-		for (double mut = 0.05; mut < max; mut = mut + step) {
-			runSingleMutatorAdj(nbTimes, randomSeed, new SimpleInversionMutator<Adjacency>(getParamsAdj(randomSeed, mut)), "simpleInversion",mut);
-			runSingleMutatorAdj(nbTimes, randomSeed, new ExchangeMutator<Adjacency>(getParamsAdj(randomSeed, mut)), "exchange",mut);
-			runSingleMutatorAdj(nbTimes, randomSeed, new InversionMutator<Adjacency>(getParamsAdj(randomSeed,mut)), "inversion",mut);
-			runSingleMutatorAdj(nbTimes, randomSeed, new InsertionMutator<Adjacency>(getParamsAdj(randomSeed,mut)), "insertion",mut);
-
-			runSingleMutatorPath(nbTimes, randomSeed, new SimpleInversionMutator<Path>(getParamsPath(randomSeed,mut)), "simpleInversion",mut);
-			runSingleMutatorPath(nbTimes, randomSeed, new ExchangeMutator<Path>(getParamsPath(randomSeed,mut)), "exchange",mut);
-			runSingleMutatorPath(nbTimes, randomSeed, new InversionMutator<Path>(getParamsPath(randomSeed,mut)), "inversion",mut);
-			runSingleMutatorPath(nbTimes, randomSeed, new InsertionMutator<Path>(getParamsPath(randomSeed,mut)), "insertion",mut);
+		System.out.println("seed: " + randomSeed);
+		for (double mutPerc = 0.0; mutPerc < max; mutPerc = mutPerc + step) {
+			long beforeIt = System.currentTimeMillis();
+			System.out.print("1: ");
+			runSingleMutatorAdj(nbTimes, randomSeed, new SimpleInversionMutator<Adjacency>(getParamsAdj(randomSeed, mutPerc)), "simpleInversion",mutPerc);
+			System.out.print("2: ");
+			runSingleMutatorAdj(nbTimes, randomSeed, new ExchangeMutator<Adjacency>(getParamsAdj(randomSeed, mutPerc)), "exchange",mutPerc);
+			System.out.print("3: ");
+			runSingleMutatorAdj(nbTimes, randomSeed, new InversionMutator<Adjacency>(getParamsAdj(randomSeed,mutPerc)), "inversion",mutPerc);
+			System.out.print("4: ");
+			runSingleMutatorAdj(nbTimes, randomSeed, new InsertionMutator<Adjacency>(getParamsAdj(randomSeed,mutPerc)), "insertion",mutPerc);
+			System.out.print("5: ");
+			runSingleMutatorPath(nbTimes, randomSeed, new SimpleInversionMutator<Path>(getParamsPath(randomSeed,mutPerc)), "simpleInversion",mutPerc);
+			System.out.print("6: ");
+			runSingleMutatorPath(nbTimes, randomSeed, new ExchangeMutator<Path>(getParamsPath(randomSeed,mutPerc)), "exchange",mutPerc);
+			System.out.print("7: ");
+			runSingleMutatorPath(nbTimes, randomSeed, new InversionMutator<Path>(getParamsPath(randomSeed,mutPerc)), "inversion",mutPerc);
+			System.out.print("8: ");
+			runSingleMutatorPath(nbTimes, randomSeed, new InsertionMutator<Path>(getParamsPath(randomSeed,mutPerc)), "insertion",mutPerc);
+			double timeIt = System.currentTimeMillis() - beforeIt;
+			double hoursIt = timeIt / (60000*60);
+			double minsIt = timeIt / (60000);
+			System.out.println("exec time of one iteration: " + hoursIt + " hours, " + minsIt + " mins");
 		}
 
+		double time = System.currentTimeMillis() - before;
+		double hours = time / (60000*60);
+		double mins = time / (60000);
+		
+		System.out.println("execution time in hours: " + hours);
+		System.out.println("execution time in mins: " + mins);
 	}
 
 	private static void runSingleMutatorAdj(int nbTimes, int randomSeed, Mutator<Adjacency> mut, String name, double mutPerc) {
-		History history = new History(outputAdjacency + name + ".txt");
+		History history = new History(outputAdjacency + name + mutPerc + ".txt");
 		createGAAdj(problem, getParamsAdj(randomSeed, mutPerc), mut).run(problem, history, nbTimes);
 		history.writeFile();
+		history.printShort();
 	}
 
 	private static void runSingleMutatorPath(int nbTimes, int randomSeed, Mutator<Path> mut, String name, double mutPerc) {
-		History history = new History(outputPath + name + ".txt");
+		History history = new History(outputPath + name + mutPerc + ".txt");
 		createGAPath(problem, getParamsPath(randomSeed, mutPerc), mut).run(problem, history, nbTimes);
 		history.writeFile();
+		history.printShort();
 	}
 
 	private static Params getParamsPath(int seed, double mut) {
-		params.popSize = 150;
-		params.maxGenerations = 100;
+		Params params = getParams(seed);
 		params.elitists = 0.05d;
-		params.crossover = 0.95d;
+		params.crossover = 0.90d;
 		params.mutation = mut;
-		params.stop = 0.95d;
-		params.detectLoops = false;
-		params.rand = new Random(seed);
-		params.correlativeTournament = false;
-		params.similarSubsetSize = 1.0;
 		return params;
 	}
 
 	private static Params getParamsAdj(int seed, double mut) {
-		params.popSize = 150;
-		params.maxGenerations = 100;
+		Params params = getParams(seed);
 		params.elitists = 0.05d;
 		params.crossover = 0.20d;
 		params.mutation = mut;
-		params.stop = 0.95d;
+		return params;
+	}
+	
+	private static Params getParams(int seed) {
+		Params params = new Params();
+		params.popSize = 150;
+		params.maxGenerations = 200;
 		params.detectLoops = false;
 		params.rand = new Random(seed);
 		params.correlativeTournament = false;
 		params.similarSubsetSize = 1.0;
+		params.stop = 0.95d;
 		return params;
 	}
 
