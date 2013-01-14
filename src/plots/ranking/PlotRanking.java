76f5233173bdc0e84a9b8448dac8e21bfa2d15a7
Kristof Peeters <circafreak@hotmail.com>
2013-01-14 05:37:11 +0100

    plot ranking speciaal voor david

--- src/plots/ranking/PlotRanking.java
+++ src/plots/ranking/PlotRanking.java
@@ -1,7 +1,5 @@
 package plots.ranking;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import main.GA;
@@ -11,10 +9,11 @@ import main.crossover.AlternatingEdge;
 import main.crossover.EdgeRecombination;
 import main.insertion.FBI;
 import main.mutation.SimpleInversionMutator;
+import main.rankers.FitnessRanker;
 import main.rankers.LineairRanker;
+import main.rankers.NonLinearRanker;
 import main.rankers.Ranker;
 import main.selectors.SUS;
-import main.selectors.Tournament;
 import params.Params;
 import representations.Adjacency;
 import representations.path.Path;
@@ -27,71 +26,135 @@ public class PlotRanking {
 	private static final String problemFilePath = "../genetic/datafiles/xqf131.tsp";
 	private static final Problem problem = ProblemGenerator.generate(problemFilePath);
 
-	private static final String outputAdjRWSFilePath = "../genetic/result/result-adj-rws.txt";
-	private static final String outputAdjSUSFilePath = "../genetic/result/result-adj-sus.txt";
-	private static final String outputAdjTournamentFilePath = "../genetic/result/result-adj-tournament.txt";
-	
-	private static final String outputPathRWSFilePath = "../genetic/result/result-path-rws.txt";
-	private static final String outputPathSUSFilePath = "../genetic/result/result-path-sus.txt";
-	private static final String outputPathTournamentFilePath = "../genetic/result/result-path-tournament.txt";
-	
+	private static final String outputAdjLinearRankerFilePath = "../genetic/result/result-adj-lr";
+	private static final String outputAdjNonLinearRankerFilePath = "../genetic/result/result-adj-nr";
+	private static final String outputAdjFitnessRankerFilePath = "../genetic/result/result-adj-fr.txt";
+
+	private static final String outputPathLinearRankerFilePath = "../genetic/result/result-path-lr";
+	private static final String outputPathNonLinearRankerFilePath = "../genetic/result/result-path-nr";
+	private static final String outputPathFitnessRankerFilePath = "../genetic/result/result-path-fr.txt";
+
 	public static void main(String[] args) {
 		makePlot();
 	}
-	
+
 	public static void makePlot() {
-		
-		int nbTimes = 10;
-		
+
+		int nbTimes = 25;
+
 		long before = System.currentTimeMillis();
-		
-		List<Double> selectivePressures = new ArrayList<Double>();
-		
+
 		int randomSeed = (new Random()).nextInt();
+
+
+		System.out.println("adjacency fitness ranker: ");
+		System.out.println();
 		
-		
-		History history1 = new History(outputAdjRWSFilePath);
-		createGA(problem, new LineairRanker<Adjacency>(), getParamsAdj(randomSeed)).run(problem, history1, nbTimes);
+		History history1 = new History(outputAdjFitnessRankerFilePath);
+		createGA(problem, new FitnessRanker<Adjacency>(), getParamsAdj(randomSeed)).run(problem, history1, nbTimes);
 		history1.printShort();
 		history1.writeFile();
+
+		System.out.println();
+		System.out.println("adjacency linear ranker");
+		System.out.println();
 		
-		History history2 = new History(outputAdjSUSFilePath);
-		createGAPath(problem, new LineairRanker<Path>(), getParamsPath(randomSeed)).run(problem, history2, nbTimes);
-		history2.printShort();
-		history2.writeFile();
+		double pressure = 2;
+		//for (double pressure = 1; pressure <= 2.2; pressure = pressure + 0.2) {
+			System.out.println("pressure: " + pressure);
+			long s = Math.round(pressure*100);
+			History history2 = new History(outputAdjLinearRankerFilePath + "-" + s +  ".txt");
+			createGA(problem, new LineairRanker<Adjacency>(pressure), getParamsAdj(randomSeed)).run(problem, history2, nbTimes);
+			history2.printShort();
+			history2.writeFile();
+			System.out.println();
+		//}
+
+		System.out.println();
+		System.out.println("adjacency nonlinear ranker");
+		System.out.println();
+		
+		int pressureInt = 21;
+		//for (int pressure = 1; pressure <= 30; pressure = pressure + 5) {
+			System.out.println("pressure: " + pressure);
+			History history3 = new History(outputAdjNonLinearRankerFilePath + "-" + Math.round(pressure*100) +  ".txt");
+			createGA(problem, new NonLinearRanker<Adjacency>(pressureInt, getParamsAdj(randomSeed)), getParamsAdj(randomSeed)).run(problem, history3, nbTimes);
+			history3.printShort();
+			history3.writeFile();
+			System.out.println();
+		//}
+
+//		System.out.println();
+//		System.out.println("path fitness ranking");
+//		System.out.println();
+//		
+//		History history4 = new History(outputPathFitnessRankerFilePath);
+//		createGAPath(problem, new FitnessRanker<Path>(), getParamsPath(randomSeed)).run(problem, history4, nbTimes);
+//		history4.printShort();
+//		history4.writeFile();
+//
+//		System.out.println();
+//		System.out.println("path linear ranking");
+//		System.out.println();
+//		
+//		//for (double pressure = 1; pressure <= 2.2; pressure = pressure + 0.2) {
+//			System.out.println("pressure: " + pressure);
+//			History history5 = new History(outputPathLinearRankerFilePath + "-" + Math.round(pressure*100) +  ".txt");
+//			createGAPath(problem, new LineairRanker<Path>(pressure), getParamsPath(randomSeed)).run(problem, history5, nbTimes);
+//			history5.printShort();
+//			history5.writeFile();
+//			System.out.println();
+//		//}
+//
+//		System.out.println();
+//		System.out.println("path nonlinear ranking");
+//		System.out.println();
+//		
+////		for (int pressure = 1; pressure <= 30; pressure = pressure + 5) {
+//			System.out.println("pressure: " + pressure);
+//			History history6 = new History(outputPathNonLinearRankerFilePath + "-" + Math.round(pressure*100) +  ".txt");
+//			createGAPath(problem, new NonLinearRanker<Path>(pressureInt,getParamsPath(randomSeed)), getParamsPath(randomSeed)).run(problem, history6, nbTimes);
+//			history6.printShort();
+//			history6.writeFile();
+//			System.out.println();
+////		}
+//
+//		double time = System.currentTimeMillis() - before;
+//		double mins = time / (60000);
+//		System.out.println("total time in mins: " + mins);
 		
 	}
 
 	private static Params getParams(int seed) {
 		Params params = new Params();
 		params.rand =  new Random(seed);
-		params.popSize = 150;
-		params.maxGenerations = 400;
+		params.popSize = 50;
+		params.maxGenerations = 500;
 		params.stop = 0.95d;
 		params.detectLoops = false;
 		return params;
 	}
-	
+
 	private static Params getParamsPath(int seed) {
 		Params params = getParams(seed);
-		params.elitists = 0.05d;
-		params.crossover = 0.90d;
-		params.mutation = 0.20d;
+		params.elitists = 0.2d;
+		params.crossover = 0.75d;
+		params.mutation = 0.40d;
 		return params;
 	}
-	
+
 	private static Params getParamsAdj(int seed) {
 		Params params = getParams(seed);
 		params.elitists = 0.2d;
 		params.crossover = 0.25d;
-		params.mutation = 0.35d;
+		params.mutation = 0.40d;
 		return params;
 	}
-	
+
 	public static GA<Adjacency> createGA(Problem problem, Ranker<Adjacency> ranker, Params params){
 		return new GA<Adjacency>(params, new AdjacencyFactory(), new SUS<Adjacency>(params), new AlternatingEdge(params, problem), new FBI<Adjacency>(params), new SimpleInversionMutator<Adjacency>(params), ranker, null);
 	}
-	
+
 	public static GA<Path> createGAPath(Problem problem, Ranker<Path> ranker, Params params){
 		return new GA<Path>(params, new PathFactory(), new SUS<Path>(params), new EdgeRecombination(problem,params), new FBI<Path>(params), new SimpleInversionMutator<Path>(params), ranker, null);
 	}
